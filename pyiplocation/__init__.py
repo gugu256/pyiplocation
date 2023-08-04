0d64d07575bfa293c0a7bd37c9cf774a5eccca66
gugu256 <gugu256@mail.com>
2023-08-04 14:40:42 +0200

    API finished, TODO: package

--- pyiplocation/__init__.py
+++ pyiplocation/__init__.py
@@ -8,16 +8,18 @@ def geolocate(ip: str):
         "hostname": page[152].replace("<td>", "").replace("</td>", ""),
         "continent_code": page[156].replace("<td>", "").replace("</td>", ""),
         "continent_name": page[160].replace("<td>", "").replace("</td>", ""),
-        "country_codes": {"alpha-2": page[164].replace("<td>", "").replace("</td>", ""), "alpha-3": page[168].replace("<td>", "").replace("</td>", "")},
+        "country_codes": {
+            "alpha-2": page[164].replace("<td>", "").replace("</td>", ""), 
+            "alpha-3": page[168].replace("<td>", "").replace("</td>", "")},
         "country_name": page[172].replace("<td>", "").replace("</td>", ""),
         "country_capital": page[182].replace("<td>", "").replace("</td>", ""),
         "state_province": page[186].replace("<td>", "").replace("</td>", ""),
         "district_county": page[190].replace("<td>", "").replace("</td>", ""),
         "city": page[194].replace("<td>", "").replace("</td>", ""),
         "zip_code": page[198].replace("<td>", "").replace("</td>", ""),
-        "latitude": page[202].replace("<td>", "").replace("</td>", "").split(", ")[0],
-        "longitude": page[202].replace("<td>", "").replace("</td>", "").split(", ")[1],
-        "geoname_id": page[206].replace("<td>", "").replace("</td>", ""),
+        "latitude": float(page[202].replace("<td>", "").replace("</td>", "").split(", ")[0]),
+        "longitude": float(page[202].replace("<td>", "").replace("</td>", "").split(", ")[1]),
+        "geoname_id": int(page[206].replace("<td>", "").replace("</td>", "")),
         "is_eu": True if page[210].replace("<td>", "").replace("</td>", "") == "true" else False,
         "calling_code": page[214].replace("<td>", "").replace("</td>", ""),
         "country_tld": page[218].replace("<td>", "").replace("</td>", ""),
@@ -26,12 +28,17 @@ def geolocate(ip: str):
         "connection_type": page[230].replace("<td>", "").replace("</td>", ""),
         "organization": page[234].replace("<td>", "").replace("</td>", ""),
         "AS_number": page[238].replace("<td>", "").replace("</td>", ""),
-        "currency": page[242].replace("<td>", "").replace("</td>", ""),
-        "currency_code": page[247][-7:].replace("<td>", "").replace("</td>", "").replace("</a>", ""),
-        "currency_symbol": page[252].replace("<td>", "").replace("</td>", ""),
-        "timezone": page[256].replace("<td>", "").replace("</td>", ""),
-        "timezone_offset": page[260].replace("<td>", "").replace("</td>", ""),
-        # kinok
+        "currency": {
+            "name": page[242].replace("<td>", "").replace("</td>", ""),
+            "code": page[247][-7:].replace("<td>", "").replace("</td>", "").replace("</a>", ""),
+            "name": page[252].replace("<td>", "").replace("</td>", "")},
+        "timezone": {
+            "name": page[256].replace("<td>", "").replace("</td>", ""),
+            "offset": float(page[260].replace("<td>", "").replace("</td>", "")),
+            "current_time": page[264].replace("<td>", "").replace("</td>", ""),
+            "current_time_unix": page[268].replace("<td>", "").replace("</td>", ""),
+            "is_DST": True if page[272].replace("<td>", "").replace("</td>", "") == "true" else False,
+            "DST_savings": float(page[276].replace("<td>", "").replace("</td>", ""))}
     }
 
-print(geolocate("12.34.5.6"))
+print(geolocate("79.95.87.19"))
